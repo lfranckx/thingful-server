909a49502c24044072f9f554559bd77f3f2be243
Bowie <bowie@thinkful.com>
2020-03-11 14:23:53 -0700

    update

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -21,6 +21,8 @@ authRouter
         loginUser.user_name
     )
         .then(dbUser => {
+            console.log(dbUser);
+            
             if (!dbUser)
                 return res.status(400).json({
                     error: 'Incorrect user_name or password',
--- src/users/users-router.js
+++ src/users/users-router.js
@@ -47,8 +47,6 @@ usersRouter
                                             .json(UsersService.serializeUser(user))
                                     })
                             })
-                    
-                    
                 })
                 .catch(next)
   })
